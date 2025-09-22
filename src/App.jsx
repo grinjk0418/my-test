96db03777c5c865e5a8810f028dd1961adf70c6d
grinjk0418 <grinjk0418@gmail.com>
2025-09-22 18:10:12 +0900

    250922 버스정류장 리스트 페이지 연습

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/common/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <h1>정류장 리스트</h1>  
+    </>
+  )
+}
+
+export default Header;
